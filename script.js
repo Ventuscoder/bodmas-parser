06974f3082e4d49fa3f17b62bffda781120b5330
ventuscoder <shivankcode@gmail.com>
2020-12-24 23:09:54 +0530

    Created function for checking errors in expression string

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const res = document.querySelector('.res');
 const inputBox = document.querySelector('main-input-box');
 const evalBtn = document.querySelector('eval-btn');
+
+function checkErr(str) {
+    const allowedValues = ['1234567890()+-*/'];
+    for (char in str) {
+        if (allowedValues.includes(char)) {
+            continue;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
